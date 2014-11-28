133d9106245d4bee52ca6e7f63b67e53a6093a3f
Anton Avramov <lukav@lukav.com>
2014-11-29 00:36:11 +0200

    Initial methods domains.available domains.register

---
+++ resellerclub.js
@@ -0,0 +1,96 @@
+var querystring = require("querystring");
+var https = require('https');
+
+var ResellerClub = function(userId, key, test) {
+  this.userId = userId;
+  this.key = key;
+  this.test = (test)?true:false;
+  //var self = this;
+  this.domains = new Domains(this);
+}
+
+Domains = function(parent){
+  this.parent = parent;
+}
+
+ResellerClub.prototype._request = function(method, path, data, callback, args) {
+  if (data) {
+    contentLength = data.length;
+  } else {
+    contentLength = 0;
+  }
+  var options = {
+    host: ((this.test)?'test.':'')+'httpapi.com',
+    path: path,
+    method: method,
+    headers: {
+      'User-Agent': 'Mozilla/4.0 (Node.js ResellerClub client)',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': contentLength
+    }
+  };
+  var req = https.request(options, function(res) {
+    res.setEncoding('utf8');
+    var buffer = '';
+    res.on('data', function(data) {
+      buffer += data;
+    });
+    res.on('end', function() {
+      try {
+        var json = JSON.parse(buffer);
+      } catch (err) {
+        return callback(err);
+      }
+      if (json.error) {
+        callback(json.error);
+      } else {
+        callback(null, json);
+      }
+    });
+  });
+  req.on('error', function(err) {
+    callback(err);
+  });
+  req.setTimeout(60000, function() {
+    req.abort();
+  });
+  req.end(data);
+}
+
+ResellerClub.prototype._get = function(apiClass, method, callback, args) {
+  var path = '/api/' + apiClass + '/' + method + '.json';
+  args['auth-userid'] = this.userId;
+  args['api-key'] = this.key;
+  path += '?' + querystring.stringify(args);
+  this._request('get', path, undefined, callback, args);
+}
+
+ResellerClub.prototype._post = function(apiClass, method, callback, args) {
+  var path = '/api/' + apiClass + '/' + method + '.json';
+  args['auth-userid'] = this.userId;
+  args['api-key'] = this.key;
+  var data = querystring.stringify(args);
+  console.log(data);
+  this._request('post', path, data, callback, args);
+}
+
+// 
+// Domains API
+// 
+
+Domains.prototype.available = function(callback, domainNames, tlds, alternatives) {
+  this.parent._get('domains', 'available', callback, {'domain-name':domainNames,'tlds':tlds,'suggest-alternative':(alternatives)?'true':'false'});
+}
+
+Domains.prototype.register = function(callback, domainName, years, ns, customerId, contacts, invoice) {
+  var args = {'domain-name':domainName, 'years':years, 'ns':ns, 'customer-id':customerId, 'invoice-option':invoice};
+  if (parseInt(contacts)){
+    var carr = ['reg-contact-id','admin-contact-id','tech-contact-id','billing-contact-id'];
+    for(f in carr){
+      args[carr[f]] = contacts;
+    }
+  }
+  this.parent._post('domains', 'register', callback, args);
+}
+
+module.exports = ResellerClub;
